986d8329bfbea12e1dc24a53764d6f34196529bd
kyuki3rain <kyuki3rain>
2022-09-11 01:01:19 +0900

    add SvgObject

---
+++ src/components/shared/SvgObject.tsx
@@ -0,0 +1,28 @@
+import { usePoint } from "../../hooks/usePoint";
+import { useSvgObject } from "../../states/svgObjectState";
+
+type Props = {
+  svgId: SvgId;
+};
+
+const SvgObject: React.FC<Props> = ({ svgId }) => {
+  const { svgObject: obj } = useSvgObject(svgId);
+  const { toReal } = usePoint();
+
+  if (!obj) return null;
+
+  switch (obj.type) {
+    case "line": {
+      const r1 = toReal(obj.point1);
+      const r2 = toReal(obj.point2);
+      return (
+        <line x1={r1.x} y1={r1.y} x2={r2.x} y2={r2.y} {...obj.style}></line>
+      );
+    }
+    case "text": {
+      return <text></text>;
+    }
+  }
+};
+
+export default SvgObject;
