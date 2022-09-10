9d92cc5651555fa9903d9cae2d3546d3707722e9
kyuki3rain <kyuki3rain>
2022-09-10 14:35:36 +0900

    add usePoint & fix limitValue test

--- src/helpers/limitValue.test.ts
+++ src/helpers/limitValue.test.ts
@@ -6,10 +6,10 @@ describe("limitValue", () => {
       expect(getLimitedValue(-10, -4, 20)).toEqual(-4);
     });
     test("no limit", () => {
-      expect(getLimitedValue(125, 100, 200)).toEqual(-4);
+      expect(getLimitedValue(125, 100, 200)).toEqual(125);
     });
     test("over limit", () => {
-      expect(getLimitedValue(30, 0, 15)).toEqual(-4);
+      expect(getLimitedValue(30, 0, 15)).toEqual(15);
     });
   });
 });
---
+++ src/hooks/usePoint.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jest-environment-jsdom
+ */
+
+import { renderHook } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import { usePoint } from "./usePoint";
+import * as rp from "../helpers/realPoint";
+import * as vp from "../helpers/virtualPoint";
+
+describe("usePoint", () => {
+  const { result } = renderHook(() => usePoint(), { wrapper: RecoilRoot });
+
+  test("toVirtual", () => {
+    const v = vp.create(3, 4);
+    expect(result.current.toReal(v)).toEqual(rp.create(30, 40));
+  });
+
+  test("toReal", () => {
+    const r = rp.create(10, 20);
+    expect(result.current.toVirtual(r)).toEqual(vp.create(1, 2));
+  });
+});
