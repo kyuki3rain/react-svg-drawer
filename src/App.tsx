a4aec2159c88e87dbcdd76b60bb3bced2e573523
kyuki3rain <kyuki3rain>
2022-09-10 14:42:04 +0900

    cssBaselineを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import DrawArea from "./components/DrawArea";
 
 const App: React.FC = () => (
   <RecoilRoot>
+    <CssBaseline />
     <DrawArea />
   </RecoilRoot>
 );
