8a414cca7d8aba7bb8a6791b3084cdae43bd9515
kyuki3rain <kyuki3rain>
2022-09-17 00:44:11 +0900

    areaConfigStateを解体

--- src/hooks/useZoom.ts
+++ src/hooks/useZoom.ts
@@ -1,25 +1,62 @@
-import { useSetAreaConfig } from "../states/areaConfigState";
-import * as rp from "../helpers/realPoint";
+import {
+  areaConfigState,
+  PITCH_MAX,
+  PITCH_MIN,
+} from "../states/areaConfigState";
 import { useCallback } from "react";
+import { useRecoilCallback } from "recoil";
+import { getLimitedValue } from "../helpers/limitValue";
+import * as vp from "../helpers/virtualPoint";
+import * as rp from "../helpers/realPoint";
 
 export const useZoom = () => {
-  const { incPitch, incPitchWithCorrect, decPitch, decPitchWithCorrect } =
-    useSetAreaConfig();
+  const setPitchRelative = useRecoilCallback(
+    ({ set }) =>
+      (relativePitch: number) => {
+        set(areaConfigState, (prev) => ({
+          ...prev,
+          pitch: getLimitedValue(
+            prev.pitch + relativePitch,
+            PITCH_MIN,
+            PITCH_MAX
+          ),
+        }));
+      }
+  );
+
+  const setPitchRleativeWithCorrect = useRecoilCallback(
+    ({ set }) =>
+      (relativePitch: number, r: RealPoint) => {
+        set(areaConfigState, (prev) => {
+          const newPitch = getLimitedValue(
+            prev.pitch + relativePitch,
+            PITCH_MIN,
+            PITCH_MAX
+          );
+          const oldv = rp.toVirtual(r, prev.pitch, prev.upperLeft);
+          const newv = rp.toVirtual(r, newPitch, prev.upperLeft);
+          return {
+            pitch: newPitch,
+            upperLeft: vp.sub(prev.upperLeft, vp.sub(newv, oldv)),
+          };
+        });
+      }
+  );
 
   const zoomIn = useCallback(
     (x?: number, y?: number) => {
-      if (!x || !y) incPitch();
-      else incPitchWithCorrect(rp.create(x, y));
+      if (!x || !y) setPitchRelative(1);
+      else setPitchRleativeWithCorrect(1, rp.create(x, y));
     },
-    [incPitch, incPitchWithCorrect]
+    [setPitchRelative, setPitchRleativeWithCorrect]
   );
 
   const zoomOut = useCallback(
     (x?: number, y?: number) => {
-      if (!x || !y) decPitch();
-      else decPitchWithCorrect(rp.create(x, y));
+      if (!x || !y) setPitchRelative(-1);
+      else setPitchRleativeWithCorrect(-1, rp.create(x, y));
     },
-    [decPitch, decPitchWithCorrect]
+    [setPitchRelative, setPitchRleativeWithCorrect]
   );
 
   return {
