7567759a54e7748a85a2c16938ffcb3baca5038a
kyuki3rain <kyuki3rain>
2022-09-19 07:51:48 +0900

    fix setlog effect

--- src/controllers/EffectController.tsx
+++ src/controllers/EffectController.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useResetPreviewEffects } from "../effects/useResetPreviewEffects";
+import { useSetLogEffects } from "../effects/useSetLogEffect";
 
 const EffectController: React.FC = () => {
   useResetPreviewEffects();
+  useSetLogEffects();
 
   return <div></div>;
 };
