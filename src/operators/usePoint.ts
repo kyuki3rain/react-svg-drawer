603387454b53f922a29ea453316f216336ecb8b3
kyuki3rain <kyuki3rain>
2022-09-24 11:04:56 +0900

    usePointでareaConfigごとに変更するよう修正

--- src/operators/usePoint.ts
+++ src/operators/usePoint.ts
@@ -1,31 +1,30 @@
-import { useRecoilCallback } from "recoil";
+import { useCallback } from "react";
+import { useRecoilCallback, useRecoilValue } from "recoil";
 import { rp } from "../helpers/realPoint";
 import { vp } from "../helpers/virtualPoint";
 import { areaConfigState } from "../states/areaConfigState";
 import { snapGridState } from "../states/snapGridState";
 
 export const usePoint = () => {
+  const areaConfig = useRecoilValue(areaConfigState);
   const toVirtual = useRecoilCallback(
     ({ snapshot }) =>
       (r: RealPoint, withSnap?: boolean) => {
         const snapGrid = snapshot.getLoadable(snapGridState).getValue();
-        const { pitch, upperLeft } = snapshot
-          .getLoadable(areaConfigState)
-          .getValue();
+        const { pitch, upperLeft } = areaConfig;
         if (withSnap === false || !snapGrid)
           return rp.toVirtual(r, pitch, upperLeft);
         return rp.toVirtualWithSnap(r, pitch, upperLeft);
       },
-    []
+    [areaConfig]
   );
 
-  const toReal = useRecoilCallback(
-    ({ snapshot }) =>
-      (v: VirtualPoint) => {
-        const { pitch } = snapshot.getLoadable(areaConfigState).getValue();
-        return vp.toReal(v, pitch);
-      },
-    []
+  const toReal = useCallback(
+    (v: VirtualPoint) => {
+      const { pitch } = areaConfig;
+      return vp.toReal(v, pitch);
+    },
+    [areaConfig]
   );
 
   return {
