ef9df238c293a3001c1b3fe5ae3bc5c4c0836d8f
kyuki3rain <kyuki3rain>
2022-10-21 14:44:24 +0900

    debug

--- src/controllers/EffectController.tsx
+++ src/controllers/EffectController.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useDebugInfoEffect } from "../effects/useDebugInfoEffect";
 import { useHandleResizeEffect } from "../effects/useHandleResizeEffect";
 import { useResetPreviewEffect } from "../effects/useResetPreviewEffects";
 import { useSetLogEffect } from "../effects/useSetLogEffect";
@@ -7,6 +8,7 @@ const EffectController: React.FC = () => {
   useResetPreviewEffect();
   useSetLogEffect();
   useHandleResizeEffect();
+  useDebugInfoEffect();
 
   return <div></div>;
 };
