b32a3057ab90a4525bdc55441f3baf269a25aeb0
kyuki3rain <kyuki3rain>
2022-10-21 14:47:33 +0900

    fix debug mode

--- src/controllers/EffectController.tsx
+++ src/controllers/EffectController.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDebugInfoEffect } from "../effects/useDebugInfoEffect";
 import { useHandleResizeEffect } from "../effects/useHandleResizeEffect";
 import { useResetPreviewEffect } from "../effects/useResetPreviewEffects";
 import { useSetLogEffect } from "../effects/useSetLogEffect";
@@ -8,7 +7,6 @@ const EffectController: React.FC = () => {
   useResetPreviewEffect();
   useSetLogEffect();
   useHandleResizeEffect();
-  useDebugInfoEffect();
 
   return <div></div>;
 };
