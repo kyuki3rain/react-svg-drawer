d375f5447b323e9ebaea47bb17fd5c39a3661ac5
kyuki3rain <kyuki3rain>
2022-11-01 00:05:03 +0900

    deleteno

--- src/operators/useGroupingObject.ts
+++ src/operators/useGroupingObject.ts
@@ -90,7 +90,7 @@ export const useGroupingObject = () => {
           deletePreview();
           return;
         } else {
-          deleteObject(id);
+          deleteObject(id, false);
           return id;
         }
       },
